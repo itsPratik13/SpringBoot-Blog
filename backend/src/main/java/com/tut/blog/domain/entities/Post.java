f0896c8ed2ac9363e99f97f9b2b5d16d7134aa06
Pratik Survase <survasepratik1304@gmail.com>
2026-06-21 15:06:29 +0530

    Post entity and post enum done

---
+++ backend/src/main/java/com/tut/blog/domain/entities/Post.java
@@ -0,0 +1,65 @@
+package com.tut.blog.domain.entities;
+
+import com.tut.blog.domain.PostStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String Title;
+
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String Content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) && Objects.equals(Title, post.Title) && Objects.equals(Content, post.Content) && status == post.status && Objects.equals(readingTime, post.readingTime) && Objects.equals(createdAt, post.createdAt) && Objects.equals(updatedAt, post.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, Title, Content, status, readingTime, createdAt, updatedAt);
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt=LocalDateTime.now();
+        this.updatedAt=LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected  void onUpdate(){
+        this.updatedAt=LocalDateTime.now();
+    }
+}
